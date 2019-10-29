19da1d0722b9f9e8612f9bacace49391a7baf038
tarikdzanic <41964501+tarikdzanic@users.noreply.github.com>
2019-10-28 22:28:35 -0500

    Update inletforcing.py

--- pyfr/plugins/inletforcing.py
+++ pyfr/plugins/inletforcing.py
@@ -110,9 +110,10 @@ class InletForcingPlugin(BasePlugin):
 			area[:ndims] += -np.einsum('i...,ij,jik', qwts, ones, norms)
 
 		# Current mass flow rate per area
-		self.mdot += -rhou[0]/area[0] # Negative since rhou_in normal points outwards
+		print(rhou[0], area[0])
+		self.mdot = -rhou[0]/area[0] # Negative since rhou_in normal points outwards
 
-		# Body forcing term added to maintain constant mass inflow
+		# Body forcing term added to maintain constant mass inflow  -> weight by portion of total area for parallel runs
 		ruf = intg.system.rhouforce + (area/self.area)*(1.0/intg._dt)*(self.mdotstar - 2.*self.mdot + intg.system.mdotold)
 
 		if (self.mdot/self.mdotstar > 1.1 or self.mdot/self.mdotstar < 0.9):
